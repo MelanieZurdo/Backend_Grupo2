29181e3b2d4104499d6982ced49630f9cb855077
irinaponzi <irina.ponzi@gmail.com>
2025-06-14 15:17:51 -0300

    feature Prestamo: unificacion de los endpoints get en uno solo que recibe query params para filtrado

--- routers/prestamoRoutes.js
+++ routers/prestamoRoutes.js
@@ -3,13 +3,9 @@ const controller = require('../controllers/prestamoController.js');
 
 const router = Router();
 
-router.get('/', controller.getAll);
-// todo cambiar estos gets a un solo endpoint con query params (varios filtros)
-router.get('/usuario/:idUsuario', controller.getByUsuario);
-router.get('/libro/:idLibro', controller.getByLibro);
-router.get('/activo', controller.getByEstadoPrestamo);
+router.get('/', controller.read);
 router.post('/', controller.create);
-router.put('/:idPrestamo', controller.update);
+router.patch('/:idPrestamo', controller.update);
 router.delete('/:idPrestamo', controller.remove);
 
 module.exports = router;
